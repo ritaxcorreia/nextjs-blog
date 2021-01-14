3a5da7ebc15a2dfebe31ba71cb2f60b32f8d1c02
Rita <rita@ritacorreia.co>
2021-01-14 15:14:47 +0000

    adding dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,50 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+}
+
+export default function Home({ allPostsData }) {
 	return (
 		<Layout home>
 			<Head>
 				<title>{siteTitle}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
-				<p>Hey there, I'm Rita! 👋</p>
 				<p>
-					(This is a sample website - you’ll be building a site like
-					this on{" "}
-					<a href="https://nextjs.org/learn">our Next.js tutorial</a>
-					.)
+					Hey there, I'm <strong>Rita</strong>! 👋
 				</p>
+				<p>
+					This is just a sample blog using Next.js. It uses dynamic
+					routing for each blog post, fetches data from markdown files
+					and pre-renders this index page with the blog post data, as
+					per below.
+				</p>
+			</section>
+
+			<section
+				className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+			>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+						</li>
+					))}
+				</ul>
 			</section>
 		</Layout>
 	);
